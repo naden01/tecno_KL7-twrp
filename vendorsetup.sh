17479a7badca5468750e7a05ea1620f0afa63242
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-03-11 01:35:48 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,4 @@
-git clone https://github.com/transsion-mt6789/twrp-device_transsion_mt6789-common.git -b fox_12.1 device/transsion/mt6789-common
+#!/bin/bash
+git clone https://github.com/naden01/twrp-device_transsion_mt6789-common.git -b fox_12.1 device/transsion/mt6789-common
+export OF_MAINTAINER_AVATAR="$(gettop)/device/tecno/KL7/maintainer_avatar.png"
+cp "${OF_MAINTAINER_AVATAR}" "$(gettop)/bootable/recovery/gui/theme/portrait_hdpi/images/Default/About/maintainer.png"
